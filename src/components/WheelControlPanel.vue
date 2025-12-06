942684b55fac27d46acc5aa3ecc370b4c301dd74
cwnnn <cangs026@gmail.com>
2025-12-06 06:55:42 +0300

    a small  but impotant detail

--- src/components/WheelControlPanel.vue
+++ src/components/WheelControlPanel.vue
@@ -2,9 +2,9 @@
   <!-- Aç/Kapa butonu -->
   <button
     @click="isOpen = true"
-    class="fixed bottom-6 right-6 bg-blue-600 text-white w-14 h-14 rounded-full shadow-lg hover:bg-blue-700 flex items-center is"
+    class="fixed bottom-6 right-6 bg-gray-400 text-white w-14 h-14 rounded-full shadow-lg hover:bg-purple-700 flex items-center justify-center text-3xl"
   >
-    ⚙️
+    😈
   </button>
 
   <!-- Modal -->
